273bd054a4a40f0d8dc50c4ca9d35c20eafb53fe
3JD <3jd@bglab.local>
2023-09-14 08:24:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/birdrunnergallery/Cenario.java
@@ -0,0 +1,14 @@
+package com.example.birdrunnergallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cenario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cenario);
+    }
+}
---
+++ app/src/main/java/com/example/birdrunnergallery/Musicas.java
@@ -0,0 +1,14 @@
+package com.example.birdrunnergallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Musicas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musicas);
+    }
+}
